0d5478993dad58cc0b37f05c1d4e9c29779362c8
cjluocode <cj.best10store@gmail.com>
2019-03-20 12:13:37 -0700

    add golden bridge and contact pages

--- landing_pages/urls.py
+++ landing_pages/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url
 
 from .views import (
                  home_page,
+                 golden_gate_bridge,
+                 contact,
 )
 
 urlpatterns = [
     url(r'^$', home_page, name='home-page'),
+    url(r'sf/golden-gate-bridge-tour/$', golden_gate_bridge, name='golden-gate-bridge'),
+    url(r'contact/$', contact, name='contact-page'),
 ]
--- landing_pages/views.py
+++ landing_pages/views.py
@@ -3,5 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 
+
+# Home Page
 def home_page(request):
     return render(request, 'landing_pages/home.html')
+
+
+# Golden Gate Bridge Tour Page
+
+def golden_gate_bridge(request):
+    return render(request,'landing_pages/golden_gate_bridge.html')
+
+
+# Contact Page
+
+def contact(request):
+    return render(request, 'landing_pages/contact.html')
