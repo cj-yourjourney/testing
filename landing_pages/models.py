d897e3c3ca50ec511ab16fec6f168ccc740cc2c1
cjluocode <cj.best10store@gmail.com>
2019-03-20 12:28:07 -0700

    add tour model

--- landing_pages/models.py
+++ landing_pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    name            = models.TextField(max_length=150)
+    email           = models.TextField(max_length=150)
+
+
+
+    def __str__(self):
+        return self.name
